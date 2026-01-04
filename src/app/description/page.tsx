d68b1a3b3ad15ccc4fe0579fba7dac1648ce0cfb
Nicolas Bezerra <nicolasbezerra13@gmail.com>
2026-01-04 18:44:41 +0000

    refactor: move technology logos to Description component and remove unused technologies constant

--- src/app/description/page.tsx
+++ src/app/description/page.tsx
@@ -2,10 +2,62 @@
 
 import { useTranslation } from "react-i18next";
 import LogoLoop from "../../components/logosLoop/LogoLoop";
-import { techLogos } from "../../constants/technologies";
+import {
+  SiReact,
+  SiNextdotjs,
+  SiTypescript,
+  SiTailwindcss,
+  SiNodedotjs,
+  SiPostgresql,
+  SiExpo,
+  SiDocker,
+} from "react-icons/si";
 
 export default function Description() {
   const { t } = useTranslation();
+
+  const techLogos = [
+    {
+      node: <SiReact className="text-white" />,
+      title: "React",
+      href: "https://react.dev",
+    },
+    {
+      node: <SiExpo className="text-white" />,
+      title: "Expo",
+      href: "https://expo.dev",
+    },
+    {
+      node: <SiNextdotjs className="text-white" />,
+      title: "Next.js",
+      href: "https://nextjs.org",
+    },
+    {
+      node: <SiNodedotjs className="text-white" />,
+      title: "Node.js",
+      href: "https://nodejs.org",
+    },
+    {
+      node: <SiTypescript className="text-white" />,
+      title: "TypeScript",
+      href: "https://www.typescriptlang.org",
+    },
+    {
+      node: <SiTailwindcss className="text-white" />,
+      title: "Tailwind CSS",
+      href: "https://tailwindcss.com",
+    },
+    {
+      node: <SiPostgresql className="text-white" />,
+      title: "PostgreSQL",
+      href: "https://www.postgresql.org",
+    },
+    {
+      node: <SiDocker className="text-white" />,
+      title: "Docker",
+      href: "https://www.docker.com",
+    },
+  ];
 
   return (
     <div className="flex flex-col items-center justify-center text-base md:text-lg gap-4 md:gap-10 mt-4 md:mt-16 w-full">
--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,2 +1,1 @@
-export * from "./technologies";
 export * from "./social-links";
